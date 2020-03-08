3c1a5b05774b2326fc3d161667f42d674d89baa0
StanZha <stas.zhabinskiy@gmail.com>
2020-03-07 22:16:14 -0600

    basic setup

--- src/test/groovy/stupid/simple/AppSpec.groovy
+++ src/test/groovy/stupid/simple/AppSpec.groovy
@@ -1,15 +1,34 @@
-package stupid.simple;
+package stupid.simple
 
-import spock.lang.Specification
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import spock.lang.Specification
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = App)
 class AppSpec extends Specification {
 
     @Autowired
     ApplicationContext applicationContext
+
+    def setup() {
+        println('will be executed before every test method')
+//        line below will print test method name in output
+        println(specificationContext.currentIteration.name)
+    }
+
+    def cleanup() {
+        println('will be executed after every test method')
+    }
+
+    def setupSpec() {
+        println('will be executed before the first feature method')
+    }
+
+    def cleanupSpec() {
+        println('will be executed after the first feature method')
+    }
 
     def "ApplicationContext is not null"() {
         expect:
